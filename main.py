d8c594a928d2a50b3805c83be3283cdc20917e77
Dylan <dylan.reichelt1@gmail.com>
2019-11-13 19:33:25 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+from aes_encryption import aes
+
+def main():
+    inputText = sys.argv[1]
+    binaryInput = inputText.encode("utf-8")
+    hexInput = binaryInput.hex()
+
+    #aesBody = aes("general dynamics")
+    aesBody = aes("Thats my Kung Fu")
+    aesBody.makeMatrix(hexInput)
+    
+
+
+if __name__ == '__main__':
+    main()
